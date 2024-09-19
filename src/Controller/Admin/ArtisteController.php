36e19ef780d3f44c40fc5eb440cd642bce9e47bc
«Mathisch78» <mathischab78@gmail.com>
2024-09-19 12:12:23 +0200

    vidéo 14 32min

---
+++ src/Controller/Admin/ArtisteController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Artiste;
+use App\Form\ArtisteType;
+use App\Repository\ArtisteRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class ArtisteController extends AbstractController
+{
+    #[Route('/admin/artistes', name: 'admin_artistes', methods: 'GET')]
+    public function listeArtistes(ArtisteRepository $repo, PaginatorInterface $paginator, Request $request)
+    {
+        $artistes=$paginator->paginate(
+        $repo->listeArtistesCompletePaginee(),
+        $request->query->getInt('page', 1), /*page number*/
+        9 /*limit per page*/
+        );
+        return $this->render('admin/artiste/listeArtistes.html.twig', [
+            'lesArtistes' => $artistes
+        ]);
+
+    }
+
+    #[Route('/admin/artiste/ajout', name: 'admin_artiste_ajout', methods: ['GET','POST'])]
+    public function ajoutArtiste()
+    {
+        $artiste=new Artiste();
+        $form=$this->createForm(ArtisteType::class, $artiste);
+        return $this->render('admin/artiste/formAjoutArtiste.html.twig', [
+            'formArtiste' => $form->createView()
+        ]);
+
+    }
+}
